c6955b5e8839ef2f9357b97fee4ab55e618cbb52
Giacomo Tesio <giacomo@tesio.it>
2010-09-16 22:36:28 +0200

    more checkup

--- src/sga/controllers/CheckupsController.java
+++ src/sga/controllers/CheckupsController.java
@@ -9,6 +9,7 @@ import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.List;
 import sga.Controller;
+import sga.domain.Checkup;
 import sga.domain.Patient;
 import sga.domain.User;
 import sga.views.CheckupView;
@@ -21,17 +22,38 @@ public class CheckupsController implements Controller {
     private static Command _nextCmd = new Command("Nuova", Command.SCREEN, 60);
     private static Command _prevCmd = new Command("Indietro", Command.BACK, 60);
 
-    private CheckupView _view;
+    private Checkup _selectedCheckup;
 
     public Displayable getView() {
-        if(null != _view)
-            return _view;
+        if(null != _selectedCheckup)
+            return buildCheckupView();
         return buildCheckupList();
+    }
+
+    private CheckupView buildCheckupView()
+    {
+        CheckupView v = new CheckupView();
+
+        
+
+        return v;
     }
 
     private List buildCheckupList()
     {
+
+        Patient selected = User.getInstance().getSelectedPatient();
         List l = new List("Visite", List.IMPLICIT);
+        
+        for(int i = 0; i < selected.numberOfCheckups(); ++i)
+        {
+            Checkup c = selected.getCheckup(i);
+            String cLabel = c.getDate().toString();
+            l.append(cLabel, null);
+        }
+
+        l.addCommand(_nextCmd);
+        l.addCommand(_prevCmd);
 
         return l;
     }
@@ -43,8 +65,8 @@ public class CheckupsController implements Controller {
         if(c == List.SELECT_COMMAND)
         {
             List l = (List)d;
-            selected.getCheckup(l.getSelectedIndex());
-            
+            _selectedCheckup = selected.getCheckup(l.getSelectedIndex());
+            return this;
         }
 
         return new MenuController();
--- src/sga/views/CheckupView.java
+++ src/sga/views/CheckupView.java
@@ -5,17 +5,96 @@
 
 package sga.views;
 
+import java.util.Date;
+import javax.microedition.lcdui.DateField;
 import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.TextField;
+import javax.microedition.lcdui.TextField;
 
 /**
  *
  * @author gtesio
  */
 public class CheckupView extends Form {
+    private DateField _date = new DateField("Data", DateField.DATE);
+    private TextField _weight = new TextField("Peso", "", 4, TextField.DECIMAL);
+    private TextField _speed = new TextField("Velocità", "", 4, TextField.DECIMAL);
 
     public CheckupView()
     {
         super("Visita");
+    }
+
+    public void setDate(Date date)
+    {
+        _date.setDate(date);
+    }
+
+    public Date getDate()
+    {
+        return _date.getDate();
+    }
+
+    /**
+     * @return the _weight
+     */
+    public double getWeight() {
+        String value = _weight.getString();
+        if(value == null || value == "")
+            return 0;
+        return Double.parseDouble(value);
+    }
+
+    /**
+     * @param weight the _weight to set
+     */
+    public void setWeight(double weight) {
+        _weight.setString(String.valueOf(weight));
+    }
+
+    /**
+     * @return the _growthSpeed
+     */
+    public double getGrowthSpeed() {
+        String value = _speed.getString();
+        if(value == null || value == "")
+            return 0;
+        return Double.parseDouble(value);
+    }
+
+    /**
+     * @param growthSpeed the _growthSpeed to set
+     */
+    public void setGrowthSpeed(double growthSpeed) {
+        _speed.setString(String.valueOf(growthSpeed));
+    }
+
+    /**
+     * @return the _dosage
+     */
+    public double getDosage() {
+        return _dosage;
+    }
+
+    /**
+     * @param dosage the _dosage to set
+     */
+    public void setDosage(double dosage) {
+        this._dosage = dosage;
+    }
+
+    /**
+     * @return the _enterPuberty
+     */
+    public boolean inPuberty() {
+        return _inPuberty;
+    }
+
+    /**
+     * @param enterPuberty the _enterPuberty to set
+     */
+    public void enterPuberty() {
+        this._inPuberty = true;
     }
 
 }
